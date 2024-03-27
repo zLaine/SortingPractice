fca1a6d0b16e9004cc5851e62d98e3f382572110
Zack Laine <rumtehk_uh@yahoo.com>
2024-03-27 10:17:53 -0400

    String array sorting

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -3,6 +3,8 @@ package main;
 import main.animals.Pig;
 import main.service.FileService;
 
+import java.util.Arrays;
+
 public class FarmerAlBaryardInventoryApplicationTwo {
 
 	public static void main(String[] args) {
@@ -11,18 +13,26 @@ public class FarmerAlBaryardInventoryApplicationTwo {
 		Pig[] pigs = fileService.getPigsFromFile();
 		String[] pigNames = new String[4];
 
-		for(Pig piggy : pigs){
-			System.out.println(piggy.getName() + " is " + piggy.getStatus());
-
-		}
-  		
 		// SORT the pigs by name...
-		
+
 		// 1. run a for loop on the pig array
 		// 2. In the for loop create an array called pig names, get the names of the pigs using
 		// the getName getter from the Pig class, save it into the pigNames string array
 		// 3. Sort the pig names
 		// 4. Print the sorted names to the console
+
+		for(int i = 0; i < pigNames.length; i++){
+			pigNames[i] = pigs[i].getName();
+			System.out.println(pigNames[i]);
+		}
+		System.out.println("~~~~~~");
+		Arrays.sort(pigNames);
+
+		for(String pig : pigNames){
+			System.out.println(pig);
+		}
+
+
 
 	}
 
